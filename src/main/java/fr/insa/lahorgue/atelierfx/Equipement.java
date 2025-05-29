36551b1c78edb0d0b7da167dc5ea8b1c0a49b8a0
pierr <pierr@LAPTOP-J8EHVTQT>
2025-05-29 23:37:56 +0200

    Tout marche.

--- src/main/java/fr/insa/lahorgue/atelierfx/Equipement.java
+++ src/main/java/fr/insa/lahorgue/atelierfx/Equipement.java
@@ -1,50 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package fr.insa.lahorgue.atelierfx;
 
-/**
- *
- * @author phupka01
- */
 public class Equipement {
-    //attributs
-    public String refEquipement;
-    public String dEquipement;
-    public float cout;
-    //Note : la méthode de calcul du coût sera définie par une méthode, qu'il sera possible de redéfinir dans les sous-classes Machine et Equipement !
-    //Constructeur
-    public Equipement(String refEquipement, String dEquipement) {
+    protected String refEquipement;
+    protected String designation;
+    protected float cout;
+
+    // Constructeur
+    public Equipement(String refEquipement, String designation) {
         this.refEquipement = refEquipement;
-        this.dEquipement = dEquipement;
+        this.designation = designation;
     }
-   //Getters
+
+    // Getters
     public String getRefEquipement() {
         return refEquipement;
     }
 
-    public String getdEquipement() {
-        return dEquipement;
+    public String getDesignation() {
+        return designation;
     }
 
     public float getCout() {
         return cout;
     }
-    
-    
-    //Setters
 
+    // Setters
     public void setRefEquipement(String refEquipement) {
         this.refEquipement = refEquipement;
     }
 
-    public void setdEquipement(String dEquipement) {
-        this.dEquipement = dEquipement;
+    public void setDesignation(String designation) {
+        this.designation = designation;
     }
 
     public void setCout(float cout) {
         this.cout = cout;
     }
-    //Une méthode de calcul du cout spéciale devra être ajoutée dans les classes Machine et Poste
+
+    // Méthode calcul coût (à redéfinir dans les sous-classes si besoin)
+    public float calculCout() {
+        return cout;
+    }
 }
