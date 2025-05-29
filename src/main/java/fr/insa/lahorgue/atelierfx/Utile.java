833f965b1773e10c24640e5df0bf6c2f05768f06
pierr <pierr@LAPTOP-J8EHVTQT>
2025-05-29 18:31:50 +0200

    Ajout d'une classe Utile avec les fonctions utilisable plusierus fois, comme la lecture des fichiers.

---
+++ src/main/java/fr/insa/lahorgue/atelierfx/Utile.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.lahorgue.atelierfx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author pierr
+ */
+public class Utile {
+    
+    
+     public static List<List<String>> importerMémoireMachine (String fichier) throws IOException{
+            
+           List<List<String>> tableau = new ArrayList<>();
+           
+           InputStream input = Utile.class.getResourceAsStream(fichier.startsWith("/") ? fichier : "/" + fichier);
+
+        
+        if (input==null){
+            throw new IOException("Fichier introuvable : "+fichier);
+        }
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(input))){
+            String ligne;
+            while ((ligne = reader.readLine())!=null){
+                String[] valeurs = ligne.split(",");
+                List<String>ligneListe = new ArrayList<>();
+                for (String val : valeurs) {
+                    ligneListe.add(val.trim());
+                }
+            tableau.add(ligneListe);
+            }
+        }
+    return tableau;    
+    }
+
+}
